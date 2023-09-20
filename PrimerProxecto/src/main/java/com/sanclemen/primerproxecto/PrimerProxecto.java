a7e6486ce1024d6011bd8c49dab19c132f2f9e2a
a22angelgp <a22angelgp@iessanclemente.net>
2023-09-20 09:58:53 +0200

    uwu numero 1

---
+++ PrimerProxecto/src/main/java/com/sanclemen/primerproxecto/PrimerProxecto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.sanclemen.primerproxecto;
+
+/**
+ *
+ * @author a22angelgp
+ */
+public class PrimerProxecto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
--- alan.py
+++
@@ -1,1 +0,0 @@
-print("Me gusta la banana")
